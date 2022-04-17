1cab7ee767691e4289f7eee8bb5a180fe547dbc4
Jun Gyu <43873104+jungyuko@users.noreply.github.com>
2022-04-17 17:21:25 +0900

    Update 15_3Sum.py
    브루트포스 방법 풀이 (시간초과)

---
+++ leetcode/07. 배열/15_3Sum.py
@@ -0,0 +1,24 @@
+### 브루트포스 1
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []                # 정답 
+        nums = sorted(nums)     # sorting!
+        
+        for i in range(len(nums)-2):                # 0부터 num-2까지 반복    
+            for j in range(i+1, len(nums)-1):       # i+1부터 num-1까지 반복
+                for k in range(j+1, len(nums)):     # j+1부터 num까지 반복
+                    lst = []                        # 임시 정답 조합 리스트 저장 파라미터
+                    if nums[i]+nums[j]+nums[k] == 0:    # 3 수의 합이 0이라면
+                        lst.append(nums[i])             # lst에 3 값을 저장
+                        lst.append(nums[j])
+                        lst.append(nums[k])
+                        ans.append(lst)                 # lst를 ans에 추가
+        
+        
+        # ans의 각 n번째 lst값을 tuple로 변환함
+        # set형태로 만들면 중복값이 제거
+        # 제거된 형태를 다시 list로 변환
+        ans = list(set([tuple(a) for a in ans]))        
+        
+        return ans
+
